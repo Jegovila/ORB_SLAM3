40392d9030a7c3634272131402a302a99f81c850
Dr. Javier Gómez <115826517+Jegovila@users.noreply.github.com>
2023-04-09 22:11:31 -0600

    Ahora se publica la pose de la cámara

--- Examples_old/ROS/ORB_SLAM3/src/ros_mono.cc
+++ Examples_old/ROS/ORB_SLAM3/src/ros_mono.cc
@@ -1,8 +1,22 @@
 /**
-* 
-* Adapted from ORB-SLAM3: Examples/ROS/src/ros_mono.cc
+* This file is part of ORB-SLAM3
 *
+* Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+*
+* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+* If not, see <http://www.gnu.org/licenses/>.
 */
+
+
 #include<iostream>
 #include<algorithm>
 #include<fstream>
@@ -16,6 +30,7 @@
 #include"../../../../include/System.h"
 #include "../../../../include/common.h"
 #include "common.cc"
+
 
 using namespace std;
 
@@ -32,35 +47,34 @@ public:
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "Mono");
-    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
-    if (argc > 1)
+    ros::start();
+
+    if(argc != 3)
     {
-        ROS_WARN ("Arguments supplied via command line are ignored.");
-    }
-
-    ros::NodeHandle node_handler;
-    std::string node_name = ros::this_node::getName();
-    image_transport::ImageTransport image_transport(node_handler);
-
-    std::string voc_file, settings_file;
-    node_handler.param<std::string>(node_name + "/voc_file", voc_file, "file_not_set");
-    node_handler.param<std::string>(node_name + "/settings_file", settings_file, "file_not_set");
-
-    voc_file = "/home/javier/ORB_SLAM3/Vocabulary/ORBvoc.txt";
-    settings_file = "/home/javier/ORB_SLAM3/Examples_old/Monocular/tello.yaml";
-	
-    if (voc_file == "file_not_set" || settings_file == "file_not_set")
-    {
-        ROS_ERROR("Please provide voc_file and settings_file in the launch file");       
+        cerr << endl << "Usage: rosrun ORB_SLAM3 Mono path_to_vocabulary path_to_settings" << endl;        
         ros::shutdown();
         return 1;
-    }
+    }    
+    
 
-    node_handler.param<std::string>(node_name + "/world_frame_id", world_frame_id, "map");
-    node_handler.param<std::string>(node_name + "/cam_frame_id", cam_frame_id, "camera");
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::MONOCULAR,true);
+
+    ImageGrabber igb(&SLAM);
+
+    ros::NodeHandle nodeHandler;
+    ros::Subscriber sub = nodeHandler.subscribe("/tello/image_raw", 1, &ImageGrabber::GrabImage,&igb);
+    
+    
+    
+    ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
+    std::string node_name = ros::this_node::getName();
+    image_transport::ImageTransport image_transport(nodeHandler);
+    nodeHandler.param<std::string>(node_name + "/world_frame_id", world_frame_id, "map");
+    nodeHandler.param<std::string>(node_name + "/cam_frame_id", cam_frame_id, "camera");
 
     bool enable_pangolin;
-    node_handler.param<bool>(node_name + "/enable_pangolin", enable_pangolin, true);
+    nodeHandler.param<bool>(node_name + "/enable_pangolin", enable_pangolin, true);
 
 
     // World frame orientation
@@ -68,22 +82,18 @@ int main(int argc, char **argv)
     std::string angle_names[3] = {"roll", "pitch", "yaw"};
     for (int i = 0; i < 3; i++)
     {
-        node_handler.param<double>(node_name + "/world_" + angle_names[i], rpy_rad(i), 0);
+        nodeHandler.param<double>(node_name + "/world_" + angle_names[i], rpy_rad(i), 0);
     }
-
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    sensor_type = ORB_SLAM3::System::MONOCULAR;
-    ORB_SLAM3::System SLAM(voc_file, settings_file, sensor_type, enable_pangolin);
-    ImageGrabber igb(&SLAM);
-
-    ros::Subscriber sub_img0 = node_handler.subscribe("/tello/image_raw", 1, &ImageGrabber::GrabImage, &igb);
-
-    setup_ros_publishers(node_handler, image_transport, rpy_rad);
+    
+    setup_ros_publishers(nodeHandler, image_transport, rpy_rad);
 
     ros::spin();
-   
+
     // Stop all threads
     SLAM.Shutdown();
+
+    // Save camera trajectory
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
 
     ros::shutdown();
 
@@ -103,8 +113,10 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
+
+    //mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
     
-        // ORB-SLAM3 runs in TrackMonocular()
+            // ORB-SLAM3 runs in TrackMonocular()
     Sophus::SE3f Tcc0 = mpSLAM->TrackMonocular(cv_ptr->image, cv_ptr->header.stamp.toSec());
     Sophus::SE3f Twc = (Tcc0 * Tc0w).inverse();
 
@@ -113,5 +125,5 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
     publish_ros_camera_pose(Twc, msg_time);
     publish_ros_tf_transform(Twc, world_frame_id, cam_frame_id, msg_time);
     publish_ros_tracked_mappoints(mpSLAM->GetTrackedMapPoints(), msg_time);
-
+    
 }
